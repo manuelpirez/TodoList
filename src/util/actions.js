b80c3f66105a97b3796d601c4536180b5f3efc6b
manuelpirez <manuelpirez@gmail.com>
2023-02-12 20:31:58 -0600

    common function to focus inputs

---
+++ src/util/actions.js
@@ -0,0 +1,15 @@
+// focus an input whenever it is selected
+const selectOnFocus = (node) => {
+    if (node && typeof node.select === 'function') { // make sure node is defined and has a select() method
+        const onFocus = (event) => node.select(); // event handler
+        node.addEventListener('focus', onFocus); // when node gets focus call onFocus()
+        return {
+            destroy: () => node.removeEventListener('focus', onFocus), // this will be executed when the node is removed from the DOM
+        }
+    }
+}
+
+// Focus an input on component init 
+const focusOnInit = (node) => node && typeof node.focus === 'function' && node.focus();
+
+export { focusOnInit, selectOnFocus }
