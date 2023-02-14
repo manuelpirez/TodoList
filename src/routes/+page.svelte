d02350e72e82559f438d3003320d6df0a427641e
manuel.pirez <manuel.pirez@pslgroup.com>
2023-02-14 16:02:00 -0600

    Session4: binding todos to stores.js

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,27 @@
 <script>
-	import Todos from './components/Todos.svelte';
-    import Alert from './components/Alert.svelte'
+    import Todos from "./components/Todos.svelte";
+    import Alert from "./components/Alert.svelte";
 
-	let todos = [
-		{ id: 1, name: 'Create a Svelte starter app', completed: false },
-		{ id: 2, name: 'Create your first component', completed: true },
-		{ id: 3, name: 'Complete the rest of the tutorial', completed: false }
-	];
+    import { todos } from "./../store/stores.js";
+    // First we need some way for our Todos component to give back the updated to-dos to its parent.
+    // We could emit an updated event with the list of to-dos,
+    // but it's easier just to bind the todos variable.
+    // !! to access the to-dos now we have to use the $todos reactive $store syntax.
+    $todos = [
+        { id: 1, name: "Create a Svelte starter app", completed: true },
+        { id: 2, name: "Create your first component", completed: true },
+        { id: 3, name: "Complete the rest of the tutorial", completed: false }
+    ];
+    $: console.log('todos', todos)
+    console.log('todos store - todos:', todos)
+    console.log('todos store content - $todos:', $todos)
 </script>
 
 <Alert />
 <!-- Here, we send the variable todos as a prop, {todos} is shorthand for todos={todos}. We all like writing less code -->
-<Todos {todos} />
+<!-- Session 3 (stores): First we need some way for our Todos component to give back the updated to-dos to its parent.
+    We could emit an updated event with the list of to-dos,
+    but it's easier just to bind the todos variable. <Todos bind:todos />
+    Everytime todos updates the variable it will get updated here as well-->
+<!-- Session 3 part 2 (stores): change the var to use the $ syntax-->
+<Todos bind:todos={$todos} />
--- src/store/stores.js
+++ src/store/stores.js
@@ -1,6 +1,6 @@
 // We import the writable() function from svelte/store
-import { writable } from 'svelte/store'
-
+import { writable } from 'svelte/store';
+export const todos = writable([]);
 
 // Use writable to create a new store called alert with an initial value of "Welcome to the to-do list app!". We then export the store.
-export const alert = writable('Welcome to the to-do list app!')
+export const alert = writable('Welcome to the to-do list app!');
